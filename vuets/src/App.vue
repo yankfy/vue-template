05f187f3ac0a81e24b0faf2e480115d6a9e28a4e
yankfy <yankfy@qq.com>
2018-12-18 16:54:27 +0800

    格式化问题

--- vuets/src/App.vue
+++ vuets/src/App.vue
@@ -4,7 +4,7 @@
       <router-link to="/">Home</router-link> |
       <router-link to="/about">About</router-link>
     </div>
-    <router-view/>
+    <router-view />
   </div>
 </template>
 <style lang="stylus">
@@ -17,9 +17,11 @@
 
 #nav
   padding 30px
+
   a
     font-weight bold
     color #2c3e50
+
     &.router-link-exact-active
       color #42b983
 </style>
