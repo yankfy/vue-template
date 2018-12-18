87c9dfbc1d26891601d90f47c076c501c49d1bd2
yankfy <yankfy@qq.com>
2018-12-18 18:11:36 +0800

    ts 自定义

--- vuets/src/components/HelloWorld.vue
+++ vuets/src/components/HelloWorld.vue
@@ -4,7 +4,8 @@
     <input
       v-model='test'
       type="text"
-    ></input>
+    />
+    <span>{{tests}}</span>
   </div>
 </template>
 
@@ -13,10 +14,22 @@ import { Component, Prop, Vue } from 'vue-property-decorator';
 
 @Component
 export default class HelloWorld extends Vue {
+  // Data定义 前面必须添加 public、private、protected
   public test: string = '12';
-
+  // Prop 传值过来的参数
   @Prop() private msg!: string;
-  // 初始化数据
+  // mounted 生命周期钩子
+  public mounted() {
+    this.init();
+  }
+  // 计算属性【函数里禁止使用alert】
+  public get tests() {
+    return this.test;
+  }
+  // methods方法
+  public init() {
+    this.test = '33';
+  }
 }
 </script>
 
